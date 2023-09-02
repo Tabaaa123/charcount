ce0345da5e2bce04ea6f6fcb451ed3c8f89b2235
Ana <anaagr30@gmail.com>
2023-09-02 23:59:41 +0800

    first commit

---
+++ counter.js
@@ -0,0 +1,44 @@
+// Get references to the textarea, character count, and custom popup elements
+var textarea = document.getElementById('myTextarea');
+var charCount = document.getElementById('charCount');
+var charRemaining = document.getElementById('charRemaining');
+var customPopup = document.getElementById('customPopup');
+var closePopupButton = document.getElementById('closePopup');
+
+// Set the maximum character limit
+var maxCharacters = 149; // Change this to your desired character limit
+
+// Add an event listener to the textarea
+textarea.addEventListener('input', function () {
+  // Get the current text and its length
+  var text = textarea.value;
+  var textLength = text.length;
+//   console
+  console.log(textLength);
+
+  // Update the character count element with the total characters
+  charCount.textContent = 'Total characters: ' + textLength;
+
+  // Calculate the remaining characters
+  var remainingCharacters = maxCharacters - textLength;
+
+  // Update the character remaining element
+  charRemaining.textContent = 'Characters remaining: ' + remainingCharacters;
+
+  // Check if the character limit is exceeded
+  if (textLength > maxCharacters) {
+    // Truncate the text to the maximum character limit
+    textarea.value = text.slice(0, maxCharacters);
+    textLength = maxCharacters; // Update the character count
+    charRemaining.textContent = 'Characters remaining: 0'; // Update remaining characters to 0
+
+    // Display the custom styled popup when the character limit is reached
+    customPopup.style.display = 'block';
+  }
+});
+
+// Add an event listener to the close button of the custom popup
+closePopupButton.addEventListener('click', function () {
+  // Hide the custom styled popup when the close button is clicked
+  customPopup.style.display = 'none';
+});
